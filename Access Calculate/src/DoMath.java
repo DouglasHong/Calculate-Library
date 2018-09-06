1db7f3520d5710bd87cce189cecf870bd093bb9b
DouglasHong <42446907+DouglasHong@users.noreply.github.com>
2018-09-06 09:04:32 -0700

    Initial commit

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+/* This is the client code for the Calculate Library. 
+ * Calls various methods in the Calculate Library.
+ * @author Douglas Hong
+ * @version 9/6/2018
+ * 
+ */
+public class DoMath {
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-3));
+	}
+}
---
+++ Calcuate Library/src/Calculate.java
@@ -0,0 +1,9 @@
+/* This class contains the methods to do various math-related tasks.
+ * @author Douglas Hong
+ * @version 9/6/2018
+ */
+public class Calculate {
+	public int square(int number) {
+		return number*number;
+	}
+}
