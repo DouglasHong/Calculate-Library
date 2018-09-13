bf72382dc85d647f3d6d27d960b26691e4e43bb8
APCS2nd <APCS2nd@MHS-Dreyer-08>
2018-09-13 09:49:14 -0700

    Part 1 Calculate Library

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,5 +7,13 @@
 public class DoMath {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(-3));
+		System.out.println(Calculate.cube(-3));
+		System.out.println(Calculate.average(6, 10));
+		System.out.println(Calculate.average(5, 11, 29));
+		System.out.println(Calculate.toDegrees(1.57));
+		System.out.println(Calculate.toRadians(90));
+		System.out.println(Calculate.discriminant(2, 8, 5));
+		System.out.println(Calculate.toImproperFrac(3, 1, 2));
+		System.out.println(Calculate.toMixedNum(7, 2));
 	}
 }
--- Calcuate Library/src/Calculate.java
+++ Calcuate Library/src/Calculate.java
@@ -7,4 +7,31 @@ public class Calculate {
 	public static int square(int number) {
 		return number*number;
 	}
+	public static int cube(int number) {
+		return number*number*number;
+	}
+	public static double average(double x, double y) {
+		return (x+y)/2;
+	}
+	public static double average(double x, double y, double z) {
+		return (x+y+z)/3;
+	}
+	public static double toDegrees(double radians) {
+		return (radians/3.14159)*180;
+	}
+	public static double toRadians(double angle) {
+		return (angle/180)*3.14159;
+	}
+	public static double discriminant(double a, double b, double c) {
+		return (b*b)-(4*a*c);
+	}
+	public static String toImproperFrac(int x, int y, int z) {
+		return (((x*z)+y) + "/" + z);
+	}
+	public static String toMixedNum(int x, int y) {
+		return (x/y + "_" + (x%y) + "/" + y);
+	}
+	public static String foil(int a, int b, int c, int d, int x, String e) {
+		return ("");
+	}
 }
