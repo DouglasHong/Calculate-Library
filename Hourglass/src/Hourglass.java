4a153dcf776a3f59a66e34cae26d0da79aa67474
APCS2nd <APCS2nd@MHS-Dreyer-08>
2018-10-22 09:47:31 -0700

    hourglass

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /* Prints out an hourglass using less than 11 println statement
  * 
  * @author Douglas Hong
@@ -6,40 +7,43 @@
 
 public class Hourglass {
 	public static void main (String[] args) {
-		printBase();
-		printTopHalf();
-		printMiddle();
-		printBottomHalf();
-		printBase();
+		Scanner console = new Scanner(System.in);
+		System.out.println("How many rows? ");
+		int numRows = console.nextInt();
+		printBase(numRows);
+		printTopHalf(numRows);
+		printMiddle(numRows);
+		printBottomHalf(numRows);
+		printBase(numRows);
 	}
-	public static void printBase() {
+	public static void printBase(int numRows) {
 		String base = "|";
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < numRows; i++) {
 			base += "\"";
 		}
 		System.out.println(base + "|");
 	}
-	public static void printTopHalf() {
-		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 1+i; j++) {
+	public static void printTopHalf(int numRows) {
+		for(int i = 1; i <= numRows/2 - 1; i++) {
+			for(int j = 0; j < i; j++) {
 				System.out.print(" ");
 			}
 			String line = "\\";
-			for(int k = 0; k < 8 - 2*i; k++) {
+			for(int k = 0; k < numRows - 2*i; k++) {
 				line += ":";
 			}
 			System.out.println(line + "/");
 		}
 	}
-	public static void printMiddle() {
-		for(int i = 0; i < 5; i++) {
+	public static void printMiddle(int numRows) {
+		for(int i = 0; i < numRows/2; i++) {
 			System.out.print(" ");
 		}
 		System.out.println("||");
 	}
-	public static void printBottomHalf(){
-		for(int i = 0; i < 4; i++) {
-			for(int j = 5; j > 1+i; j--) {
+	public static void printBottomHalf(int numRows){
+		for(int i = 0; i < numRows/2 - 1; i++) {
+			for(int j = numRows/2; j > 1+i; j--) {
 				System.out.print(" ");
 			}
 			String line = "/";
