9f5576de028da0214fd7c4d74d937ee96d78c376
dougl <dougl@DESKTOP-GETTEAK>
2018-10-25 22:19:34 -0700

    hourglass

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -1,5 +1,5 @@
 import java.util.*;
-/* Prints out an hourglass using less than 11 println statement
+/* Prints out an hourglass using less than 11 println/print statements (used 4 in the whole program)
  * 
  * @author Douglas Hong
  * @version 10/19/2018
@@ -8,49 +8,64 @@ import java.util.*;
 public class Hourglass {
 	public static void main (String[] args) {
 		Scanner console = new Scanner(System.in);
-		System.out.println("How many rows? ");
-		int numRows = console.nextInt();
-		printBase(numRows);
-		printTopHalf(numRows);
-		printMiddle(numRows);
-		printBottomHalf(numRows);
-		printBase(numRows);
+		String quit;
+		do {
+			System.out.println("How many characters? ");
+			int numChars = console.nextInt();
+			printBase(numChars);
+			printTopHalf(numChars);
+			printMiddle(numChars);
+			printBottomHalf(numChars);
+			printBase(numChars);
+			System.out.println("Do you want to build another hourglass? (Type \"quit\" to end)");
+			quit = console.next();
+			quit = quit.toLowerCase();
+		}while(!(quit.equals("quit")));
 	}
-	public static void printBase(int numRows) {
-		String base = "|";
-		for(int i = 0; i < numRows; i++) {
-			base += "\"";
-		}
-		System.out.println(base + "|");
+	//prints the top and bottom base of the hourglass
+	public static void printBase(int numChars) {
+		printStringOfChars("|", 1);
+		printStringOfChars("\"", numChars);
+		newLine("|");
 	}
-	public static void printTopHalf(int numRows) {
-		for(int i = 1; i <= numRows/2 - 1; i++) {
-			for(int j = 0; j < i; j++) {
-				System.out.print(" ");
-			}
-			String line = "\\";
-			for(int k = 0; k < numRows - 2*i; k++) {
-				line += ":";
-			}
-			System.out.println(line + "/");
+	//prints the top half of the hourglass
+	public static void printTopHalf(int numChars) {
+		for(int i = 1; i < numChars/2; i++) {
+			//j is looped until less than i because spaces = row number
+			printStringOfChars(" ", i);
+			printStringOfChars("\\", 1);
+			//the count is numChars-2*i because the colons will decrease by 2 each row
+			printStringOfChars(":", numChars-2*i);
+			newLine("/");
 		}
 	}
-	public static void printMiddle(int numRows) {
-		for(int i = 0; i < numRows/2; i++) {
-			System.out.print(" ");
-		}
-		System.out.println("||");
+	//prints the middle two ||
+	public static void printMiddle(int numChars) {
+		printStringOfChars(" ", numChars/2);
+		newLine("||");
 	}
-	public static void printBottomHalf(int numRows){
-		for(int i = 0; i < numRows/2 - 1; i++) {
-			for(int j = numRows/2; j > 1+i; j--) {
-				System.out.print(" ");
+	//prints the bottom half of the hourglass
+	public static void printBottomHalf(int numChars){
+		for(int i = 1; i < numChars/2; i++) {
+			//the count has to be decrementing so there will be less spaces after each row
+			printStringOfChars(" ", numChars/2 - i);
+			printStringOfChars("/", 1);
+			//the count is 2*i is so that there will be 2 more colons on each consecutive row
+			printStringOfChars(":", 2*i);
+			if(numChars % 2 != 0) {
+				printStringOfChars(":", 1);
 			}
-			String line = "/";
-			for(int k = 0; k < 2 + 2*i; k++) {
-				line += ":";
-			}
-			System.out.println(line + "\\");
+			newLine("\\");
 		}
+	}
+	//prints a row of multiple characters
+	public static void printStringOfChars(String s, int count) {
+		for(int i = 0; i < count; i++) {
+			System.out.print(s);
+		}
+	}
+	//finishes off a line with the last String and moves on to the next one
+	public static void newLine(String s) {
+		System.out.println(s);
 	}
 }
