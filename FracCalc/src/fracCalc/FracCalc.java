49d493288adaba92303ec799d9f107fea31d8ab4
dougl <dougl@DESKTOP-GETTEAK>
2018-11-15 15:15:51 -0800

    fraccalc checkpoint 1

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,10 +1,24 @@
 package fracCalc;
 import java.util.*;
+/* Does various operations (addition, subtraction, multiplication, and division) with two given fractions. 
+ * 
+ * @author Douglas Hong
+ * @version 11/15/2018
+ */
 public class FracCalc {
 
     public static void main(String[] args) {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please type the first operand(fraction), the operator (+,-,*,/), and the second operand (fraction) (Type 'quit' to quit)");
+        String expression = userInput.nextLine();
+        while(!expression.equals("quit")) {
+        	System.out.println(produceAnswer(expression));
+        	System.out.println("Please type the first operand(fraction), the operator (+,-,*,/), and the second operand (fraction) (Type 'quit' to quit)");
+        	expression = userInput.nextLine();
+        }
+        if(expression.equals("quit")) {
+        	System.out.println("Calculator stopped");
+        }
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
